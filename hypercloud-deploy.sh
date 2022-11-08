4af74f2be890cf4b2e37ba0a03d1a83baadb3761
sjoh0704 <sjoh0704@daum.net>
2022-11-08 21:04:19 +0900

    [mod] deploy.sh 수정

--- hypercloud-deploy.sh
+++ hypercloud-deploy.sh
@@ -5,5 +5,9 @@ export KUBECONFIG=/root/kubeconfig/ck1-kubeconfig.yaml # 실환경
 
 ## crd 설치 및 operator deploy
 export IMG=192.168.9.12:5000/hypercloud-multi-operator-dev:$1
+
+# image build and push
+# ./push.sh $1
+
 # kubectl create ns hypercloud5-system
 make deploy-test
